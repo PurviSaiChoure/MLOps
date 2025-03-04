902721dc8853fea211f5628bb861abdb26bdc46e
PurviSaiChoure <purvichoure2@gmail.com>
2025-03-04 21:48:50 +0530

    model trained

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.Stage_02_prep_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.Stage_03_model_trainer import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -22,6 +23,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -6,12 +6,11 @@ from pathlib import Path
 from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
 
 class PrepareBaseModel:
-    def __init__(self, config: PrepareBaseModelConfig):
+    def __init__(self, config):
         self.config = config
 
-    
     def get_base_model(self):
-        self.model = tf.keras.applications.vgg16.VGG16(
+        self.model = tf.keras.applications.ResNet50(
             input_shape=self.config.params_image_size,
             weights=self.config.params_weights,
             include_top=self.config.params_include_top
@@ -19,22 +18,21 @@ class PrepareBaseModel:
 
         self.save_model(path=self.config.base_model_path, model=self.model)
 
-
-    
     @staticmethod
     def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
         if freeze_all:
             for layer in model.layers:
-                model.trainable = False
+                layer.trainable = False
         elif (freeze_till is not None) and (freeze_till > 0):
             for layer in model.layers[:-freeze_till]:
-                model.trainable = False
+                layer.trainable = False
 
-        flatten_in = tf.keras.layers.Flatten()(model.output)
+        # Use GlobalAveragePooling instead of Flatten for better performance
+        pooling_layer = tf.keras.layers.GlobalAveragePooling2D()(model.output)
         prediction = tf.keras.layers.Dense(
             units=classes,
             activation="softmax"
-        )(flatten_in)
+        )(pooling_layer)
 
         full_model = tf.keras.models.Model(
             inputs=model.input,
@@ -49,7 +47,6 @@ class PrepareBaseModel:
 
         full_model.summary()
         return full_model
-    
 
     def update_base_model(self):
         self.full_model = self._prepare_full_model(
@@ -61,8 +58,6 @@ class PrepareBaseModel:
         )
 
         self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
-    
-
 
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
